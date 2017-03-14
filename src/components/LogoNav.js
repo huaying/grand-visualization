175166c82599a98099e7b3ef644f4efd0a21280d
Huaying Tsai <huayingt@Huayings-MBP.zyxel.com>
2017-03-14 01:52:31 -0700

    use NavLink

--- src/components/LogoNav.js
+++ src/components/LogoNav.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from '../images/landing-logo.png';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const LogoNav = () => (
   <div className="logo">
     <Link to="/"><img src={logo} alt="Logo" width="300" /></Link>
     <nav>
-      <Link to="/about">about</Link>{' | '}
-      <Link to="/projects">projects</Link>{' | '}
-      <Link to="/hire_us">hire us</Link>
+      <NavLink to="/about">about</NavLink>{' | '}
+      <NavLink to="/projects">projects</NavLink>{' | '}
+      <NavLink to="/hire_us">hire us</NavLink>
     </nav>
   </div>
 );
