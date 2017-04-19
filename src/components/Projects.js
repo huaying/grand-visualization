e100796da83430a8923943b9512abe2353d5032b
Huaying Tsai <huayingt@Huayings-MBP.zyxel.com>
2017-04-19 03:21:29 -0700

    flexbox gallery

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import '../styles/Proects.css';
 import setFullScreen from '../lib/setFullScreen';
 
-const workNumber = 43;
+const workNumber = 5;
 const Projects = () => {
   setFullScreen(false);
   return (
-    <div>
-    {[...Array(workNumber).keys()].map(i => (
-      <img src={`work/p${i + 1}.png`} role="presentation"></img>
-    ))}
+    <div className="projects">
+      <div className="grid-col">
+        <div className="grid-item">
+          <img src="work/p1.png" role="presentation"></img>
+        </div>
+        <div className="grid-item grid-row">
+          <div className="grid-item">
+            {[...Array(workNumber).keys()].map(i => (
+              <img src={`work/p${i + 2}.png`} role="presentation"></img>
+            ))}
+          </div>
+          <div className="grid-item2 grid-col">
+            <div className="grid-item">
+              <img src="work/p3.png" role="presentation"></img>
+            </div>
+            <div className="grid-item grid-row">
+              <div className="grid-item">
+                {[...Array(workNumber).keys()].map(i => (
+                  <img src={`work/p${i + 6}.png`} role="presentation"></img>
+                ))}
+              </div>
+              <div className="grid-item">
+                {[...Array(workNumber).keys()].map(i => (
+                  <img src={`work/p${i + 11}.png`} role="presentation"></img>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
