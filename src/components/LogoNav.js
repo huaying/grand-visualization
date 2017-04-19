d39818671a50841910a260deb6609d2d541b0834
Huaying Tsai <huayingt@Huayings-MBP.zyxel.com>
2017-04-19 04:59:54 -0700

    implement projects page

--- src/components/LogoNav.js
+++ src/components/LogoNav.js
@@ -3,11 +3,22 @@ import logo from '../images/landing-logo.png';
 import { Link, NavLink } from 'react-router-dom';
 
 const LogoNav = () => (
-  <div className="logo">
+  <div className="logo nav">
     <Link to="/"><img src={logo} alt="Logo" width="300" /></Link>
     <nav>
       <NavLink to="/about">about</NavLink>{' | '}
       <NavLink to="/projects">projects</NavLink>{' | '}
+      <NavLink to="/hire_us">hire us</NavLink>
+    </nav>
+  </div>
+);
+
+export const Nav = () => (
+  <div className="nav">
+    <Link className="brand" to="/"><img src={logo} alt="Logo" width="300" /></Link>
+    <nav>
+      <NavLink to="/about">about</NavLink>{' '}
+      <NavLink to="/projects">projects</NavLink>{' '}
       <NavLink to="/hire_us">hire us</NavLink>
     </nav>
   </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import '../styles/Proects.css';
+import { Nav } from './LogoNav';
 import setFullScreen from '../lib/setFullScreen';
 
-const workNumber = 5;
+const heroPhoto1 = 'work/p1.png';
+const heroPhoto2 = 'work/p3.png';
+const photoGroup1 = [2, 4, 8, 11, 14, 17, 20, 23, 25, 30, 33, 36, 39, 42];
+const photoGroup2 = [5, 9, 12, 15, 18, 21, 24, 26, 28, 31, 34, 37, 40, 43];
+const photoGroup3 = [6, 7, 10, 13, 16, 19, 22, 27, 29, 32, 35, 38, 41];
 const Projects = () => {
   setFullScreen(false);
   return (
     <div className="projects">
-      <div className="grid-col">
+      <Nav />
+      <div className="content grid-col">
         <div className="grid-item">
-          <img src="work/p1.png" role="presentation"></img>
+          <img src={heroPhoto1} role="presentation"></img>
         </div>
         <div className="grid-item grid-row">
           <div className="grid-item">
-            {[...Array(workNumber).keys()].map(i => (
-              <img src={`work/p${i + 2}.png`} role="presentation"></img>
+            {photoGroup1.map(i => (
+              <img src={`work/p${i}.png`} role="presentation"></img>
             ))}
           </div>
           <div className="grid-item2 grid-col">
             <div className="grid-item">
-              <img src="work/p3.png" role="presentation"></img>
+              <img src={heroPhoto2} role="presentation"></img>
             </div>
             <div className="grid-item grid-row">
               <div className="grid-item">
-                {[...Array(workNumber).keys()].map(i => (
-                  <img src={`work/p${i + 6}.png`} role="presentation"></img>
+                {photoGroup2.map(i => (
+                  <img src={`work/p${i}.png`} role="presentation"></img>
                 ))}
               </div>
               <div className="grid-item">
-                {[...Array(workNumber).keys()].map(i => (
-                  <img src={`work/p${i + 11}.png`} role="presentation"></img>
+                {photoGroup3.map(i => (
+                  <img src={`work/p${i}.png`} role="presentation"></img>
                 ))}
               </div>
             </div>
