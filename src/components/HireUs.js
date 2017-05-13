09adf267855af95d88718673d1eea6e07e9791f5
Huaying Tsai <huayingt@huaying-3.local>
2017-05-12 20:27:04 -0700

    add background and modify contact us

--- src/components/HireUs.js
+++ src/components/HireUs.js
@@ -13,28 +13,21 @@ const HireUs = () => {
           <div className="panel-title">Contact Us</div>
           <div className="panel-body">
             <p>
-            Do not hesitate to contact us for any inquiries,
-            we will reach back to you as soon as possible.
+            Please do not hesitate to contact us for any inquiries,
+            we will reach back to you as soon as possible with solutions
+            that suit your needs.
             </p>
             <div className="contact-info">
               <div className="row">
-                <h3 className="col right">Phone</h3>
-                <span className="col left">+1 8188356623</span>
+                P: +1 818.835.6623
               </div>
               <div className="row">
-                <h3 className="col right">Email</h3>
-                <span className="col left">
-                  <a href="mailto:sug@grandvisualization.com" rel="nofollow">
-                    sug@grandvisualization.com
-                  </a>
-                </span>
+                E: <a href="mailto:suh@grandvisualization.com" rel="nofollow">
+                  suh@grandvisualization.com
+                </a>
               </div>
               <div className="row">
-                <h3 className="col right">Address</h3>
-                <span className="col left">
-                  420E 3rd Street, Suite 1050. <br />
-                  Los Angeles, California 90013, U.S.A
-                </span>
+                A: 420 E. 3rd. Street, Suite 1015, Los Angeles, CA 90013
               </div>
             </div>
           </div>
@@ -45,7 +38,7 @@ const HireUs = () => {
             height="450"
             frameBorder="0"
             src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAjc1VIVNngxBdpjp3RZ25poJn7VPNNZo0
-              &q=420E+3rd+Street,+Suite+1050,+Los+Angeles,+CA+90013
+              &q=420E+3rd+Street,+Suite+1015,+Los+Angeles,+CA+90013
               &zoom=13"
           />
         </div>
