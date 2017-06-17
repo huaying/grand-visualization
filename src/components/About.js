c8fe52c716d721e25079d61f04267afe1dc9dcf5
Huaying Tsai <royal3501@gmail.com>
2017-06-17 04:31:49 -0700

    6.17.17 update

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,18 @@ import icon2 from '../images/icon2.png';
 import icon3 from '../images/icon3.png';
 import icon4 from '../images/icon4.png';
 import setFullScreen from '../lib/setFullScreen';
+import fb from '../images/facebook.svg';
+import ig from '../images/instagram.svg';
+import be from '../images/be.svg';
+import choose1 from '../images/choose1.png';
+import choose2 from '../images/choose2.png';
+import choose3 from '../images/choose3.png';
+import choose4 from '../images/choose4.png';
+import choose5 from '../images/choose5.png';
+import choose6 from '../images/choose6.png';
+import how1 from '../images/how1.jpg';
+import how2 from '../images/how2.jpg';
+import how3 from '../images/how3.jpg';
 import { Link } from 'react-router-dom';
 
 const photo1 = '/preview/7.jpg';
@@ -22,60 +34,66 @@ const About = () => {
     <div className="about">
       <Hero />
       <div className="content">
-        <div className="quote">
-          “Photorealistic architectural visualization expert you can trust.”
+        <div className="panel">
+          <div className="panel-body">
+            <p><strong>
+            Grand Visualization has a talented team of architectural rendering
+            specialists with over 8 years of combined experience. We pride
+            ourselves in consistently providing highest quality architectural
+            rendering services at competitive prices. Being founded by architects,
+            our expertise lies in being able to effectively communicate
+            architectural design to whomever it may be. We believe that the
+            core of design lies in articulating a cohesive narrative to a
+            larger audience.
+            </strong></p>
+            <p><strong>
+            We are willing to work with you via the Internet regardless of
+            your location.  Also, allow us to provide in-person services for
+            clients located in the Greater Los Angeles Area. We look forward
+            to helping you create stunning visualizations that will reach and
+            exceed your expectations.
+            </strong></p>
+          </div>
         </div>
         <div className="panel">
           <div className="panel-title">How we work</div>
-          <div className="panel-body">
-            <p>
-            Grand Visualization is an experienced team of visualization artists
-            specializing in architectural visualization for projects worldwide.
-            We will utilize our expertise to help craft beautiful images and
-            narratives for your design project.
-            </p>
-            <p>
-            Our new office located in Downtown Los Angeles, we are willing to
-            work with you via the Internet regardless of your location. Also,
-            allow us to provide in-person services for clients located in the
-            Greater Los Angeles Area. We can dispatch our specialists to work
-            onsite and revise at the final stage of the visualization project
-            until your satisfaction.
-            </p>
-            <p>
-            We look forward to helping you create stunning visualizations that
-            will reach and exceed your expectations.
-            </p>
-            <div className="flow">
-              <div>
-                <img src={one} alt="one" />
+          <div className="flow">
+            <div>
+              <div className="text-block">
+                <img className="flow-small" src={one} alt="one" />
                 <h3>Let us know your needs.</h3>
                 <p>
-                  Send us your documents of the unbuilt project, we will
-                  evaluate the files and give you a quote as soon as possible.
+                  Send us your documents of the unbuilt project,
+                  we will evaluate the files then give you a quote as soon as
+                  possible and the consultation is free.
                 </p>
               </div>
-              <div>
-                <img src={two} alt="two" />
+              <img className="flow-big" src={how1} />
+            </div>
+            <div>
+              <div className="text-block">
+                <img className="flow-small" src={two} alt="two" />
                 <h3>3D Model Creation</h3>
                 <p>
-                  Upon your approval and 25% down payment, we start work into
-                  it immediately. We will show you our draft views and make
-                  adjustments when the 3D model is completed.
+                  Upon your approval, our team will begin working immediately
+                  on 3d model creation. We will work with you and devise a
+                  customized timeline to ensure the model is completed to
+                  custmer satisfaction.
                 </p>
               </div>
-              <div>
-                <img src={three} alt="three" />
+              <img className="flow-big" src={how2} />
+            </div>
+            <div>
+              <div className="text-block">
+              <img className="flow-small" src={three} alt="three" />
                 <h3>Optimization & Delivery</h3>
                 <p>
-                  We will be making changes until the drawings meet your
-                  satisfaction. If you are in Greater Los Angeles area,
-                  we can dispatch our specialist to work on site.
-                  <br />
-                  After receiving your remaining payment, high resolution
-                  drawings will be delivered to you.
+                  We will be making changes until the drawings meet your satisfaction.
+                  After receiving your payment, high resolution drawings will
+                  be delivered to you.
                 </p>
               </div>
+              <img className="flow-big" src={how3} />
             </div>
           </div>
         </div>
@@ -101,6 +119,78 @@ const About = () => {
             </ul>
           </div>
         </div>
+        <div className="panel choose">
+          <div className="panel-title">Why choose us</div>
+          <div className="panel-body">
+            <div className="flow">
+              <div>
+                <img className="flow-small" src={choose1} alt="one" />
+                <h3>Opportunity to Streamline your Business</h3>
+                <p>
+                  We provide services that lower operational and labor costs for
+                  your company. By working with us, you will leverage a global knowledge base.
+                  In addition, you will not have to spend time and money hiring
+                  architects for renderings. Instead, you can focus on core business
+                  processes by delegating. Let us take care of the rest.
+                </p>
+              </div>
+              <div>
+                <img className="flow-small" src={choose2} alt="two" />
+                <h3>All-Around Professionalism</h3>
+                <p>
+                  Our team is well experienced with both larger and smaller scale
+                  projects. With graceful teamwork and coordination, we bring together
+                  diverse artists from the architecture design field to do the best
+                  work possible.
+                </p>
+              </div>
+              <div>
+                <img className="flow-small" src={choose3} alt="three" />
+                <h3>Competitive Price</h3>
+                <p>
+                  Our company has very competitive pricing, especially for a
+                  Los Angeles based firm. We take pride in consistently providing
+                  our clients with highest quality architectural rendering
+                  services at fair prices.
+                </p>
+              </div>
+            </div>
+            <div className="flow">
+              <div>
+                <img className="flow-small" src={choose4} alt="four" />
+                <h3>Flexibility</h3>
+                <p>
+                  With design projects, we completely understand the importance
+                  of time. We know that rendering production time frame can be very
+                  critical, which is why we offer flexible time plans based on
+                  your needs, regardless of the workload of the project.
+                </p>
+              </div>
+              <div>
+                <img className="flow-small" src={choose5} alt="five" />
+                <h3>Efficient Communication</h3>
+                <p>
+                  We make it easy for you to communicate with us.
+                  Since our artists come from architectural backgrounds,
+                  they can convey your ideas and technical drawings to
+                  visual art pieces precisely and efficiently. No more wasted
+                  time due to cultural and/or time differences.
+                </p>
+              </div>
+              <div>
+                <img className="flow-small" src={choose6} alt="six" />
+                <h3>Long Term Options</h3>
+                <p>
+                  If you are interested in long term cooperation with Grand
+                  Visualization, we offer custom-made business plans to generate
+                  the workflow that works just right for you.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </div>
       <div className="footer">
         <div className="image-group">
@@ -113,6 +203,17 @@ const About = () => {
           <Link to="/projects">see more >></Link>
         </div>
         <div className="copyright">
+          <div>
+            <a href="https://www.facebook.com/Grand-Visualization-485514698458079/" target="_blank">
+              <img src={fb} />
+            </a>
+            <a href="https://www.instagram.com/grandvisualization/" target="_blank">
+              <img src={ig} />
+            </a>
+            <a href="https://www.behance.net/suh6419" target="_blank">
+              <img src={be} />
+            </a>
+          </div>
           Copyright © 2017, Grand Visionary Inc. All rights reserved.
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,9 @@ import '../styles/Home.css';
 import bg from '../images/landing-hero.png';
 import prelaodImg from '../images/hero-blur.png';
 import setFullScreen from '../lib/setFullScreen';
+import fb from '../images/facebook.svg';
+import ig from '../images/instagram.svg';
+import be from '../images/be.svg';
 
 class Home extends Component {
   componentDidMount() {
@@ -20,6 +23,17 @@ class Home extends Component {
       <div className="home">
         <img src={bg} className="home-bg" alt="" />
         <LogoNav />
+        <div className="social">
+          <a href="https://www.facebook.com/Grand-Visualization-485514698458079/" target="_blank">
+            <img src={fb} />
+          </a>
+          <a href="https://www.instagram.com/grandvisualization/" target="_blank">
+            <img src={ig} />
+          </a>
+          <a href="https://www.behance.net/suh6419" target="_blank">
+            <img src={be} />
+          </a>
+        </div>
       </div>
     );
   }
